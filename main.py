bea0b15dc8a55a17ea913ab9bcc107b68678cc9e
Meghana R <meghana.r@enerman.in>
2025-06-25 18:59:11 +0530

    initial get and post method working

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+posts = []
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello!"}
+class UserPost(BaseModel):
+    name: str
+    quote: str
+
+@app.get("/posts")
+async def read_root():
+    print(str([post.model_dump() for post in posts]))
+
+@app.post("/post")
+async def create_post(post: UserPost):
+    posts.append(post)
+    print(str(post.model_dump()))  
