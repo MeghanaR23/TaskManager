766b56a7d4f4578de9163140e786cae89177ed1e
Meghana R <meghana.r@enerman.in>
2025-06-26 18:45:49 +0530

    added logic to register the user

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker, Session
+# app = FastAPI()
+
+DATABASE_URL = "sqlite:///./taskmanager.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+Base = declarative_base()
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, nullable=False, index=True)
+    password = Column(String, nullable=False)
+   
+class RegisterUser(BaseModel):
+    username: str
+    password: str    
 
 app = FastAPI()
-posts = []
+Base.metadata.create_all(bind=engine)
 
-class UserPost(BaseModel):
-    name: str
-    quote: str
-
-@app.get("/posts")
-async def read_root():
-    print(str([post.model_dump() for post in posts]))
-
-@app.post("/post")
-async def create_post(post: UserPost):
-    posts.append(post)
-    print(str(post.model_dump()))  
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+@app.post("/register")
+async def register(user: RegisterUser,db: Session = Depends(get_db)):
+    db_user = User(username=user.username, password=user.password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return {"message": "User registered successfully"}
